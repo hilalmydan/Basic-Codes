54467d4d3326ff16289805be6745ea8a0f739c01
hilalmydan <56756269+hilalmydan@users.noreply.github.com>
2020-07-02 14:46:12 +0300

    the cod of n!

---
+++ found n!.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+	
+	// write the program that found the n!
+	
+	int n,i,fac=1;
+	
+	printf("Enter the number which you want to take it's factorial\n");
+	scanf("%d",&n);
+	printf("The number that you entered is : %d\n",n);
+	
+	for(i=1; i<=n; i++)
+	{
+		fac*=i;
+	}
+	printf("The factorial of the number %d is %d",n,fac);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
