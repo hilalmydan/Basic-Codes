672c6623e237e72696b45c9c66c3980c144d4a8a
hilalmydan <56756269+hilalmydan@users.noreply.github.com>
2020-07-05 14:54:10 +0300

    Update random sayı üreten program.cpp

--- random sayı üreten program.cpp
+++ random sayı üreten program.cpp
@@ -1,6 +1,4 @@
-/* Problem : Random degerlerle olusturulacak 10×10’luk matrisi önce yazdýran 
-ve program içerisinde bu iþlemi yaparken kaç kere deðiþtirme iþlemi yapýldýðýný,
-en büyük rakamý ve kaç tane olduðunu ekrana yazdýran program kodunu yazýnýz. */ 
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
